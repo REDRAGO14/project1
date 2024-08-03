1c1b09e89737e1b65f77246aaa056a0a20059818
Nobel Markos <143226491+nobafuture@users.noreply.github.com>
2024-08-03 09:10:32 +0300

    main folder

---
+++ frontend/src/component/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'> 
+    <div className="header-contents">
+      <h2>Order Your Food Here</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, praesentium doloremque! Officiis consequatur voluptate ut aut temporibus corporis, rerum amet harum molestias qui omnis! Hic quae explicabo velit veniam quasi!</p>
+      <button>Veiw Menu</button>
+    </div>
+
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import "./Home"
+import Header from '../../component/Header/Header'
+import ExploreMenu from '../../component/ExploreMenu/ExploreMenu'
+
+const Home = () => {
+  const [category, setCategory] = useState("All");
+   return (
+    <div>
+      <Header/> 
+      <ExploreMenu category={category} setCategory={setCategory}/>
+    </div>
+  )            
+}
+
+export default Home
