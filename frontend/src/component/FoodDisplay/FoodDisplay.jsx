06a1770129be4d4a76cc2814687c8d4174628d6c
Nobel Markos <143226491+nobafuture@users.noreply.github.com>
2024-08-03 19:04:52 +0300

    3rd front end commit up to 1:59

--- frontend/src/component/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/component/FoodDisplay/FoodDisplay.jsx
@@ -10,7 +10,9 @@ const FoodDisplay = ({category}) => {
       <h2>Top Dish Near You</h2>
       <div className="food-display-list">
         {food_list.map((item, index)=>{
+          if (category==="All" || category===item.category) {
             return<FoodItem key={index} id={item._id} name={item.name} descripiton={item.descripiton} price={item.price} image={item.image} />
+          }         
         })}
       </div>
     </div>
